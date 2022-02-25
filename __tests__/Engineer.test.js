10053055b81838dae7bd5ca3a3963e3f47ce3d25
nitrotap <kartikinpublic@gmail.com>
2022-02-24 23:32:33 -0700

    adding engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('../lib/Engineer.js')
+
+test("creates engineer object", () => {
+    const engineer = new Engineer("Kartik", 1, "kartikinpublic@gmail.com", "nitrotap")
+    expect(engineer.name).toBe("Kartik")
+    expect(engineer.id).toEqual(expect.any(Number))
+    expect(engineer.email).toMatch("@")
+    expect(engineer.gitHub).toMatch("nitrotap")
+
+    console.log(engineer)
+})
+
+test("gets engineer's github information", () => {
+    const engineer = new Engineer("Kartik", 1, "kartikinpublic@gmail.com", "nitrotap")
+    expect(engineer.getGithub()).toBe("nitrotap")
+})
+
+test("gets engineer's role", () => {
+    const engineer = new Engineer("Kartik", 1, "kartikinpublic@gmail.com", "nitrotap")
+    expect(engineer.getRole()).toBe("Engineer")
+})
+
