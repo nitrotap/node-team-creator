b387819b7df2d41cd1ea0a0a3b0975bcfb67ad41
nitrotap <kartikinpublic@gmail.com>
2022-02-24 23:18:51 -0700

    pass employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,28 +1,29 @@
 const Employee = require('../lib/Employee.js')
 
-test('creates employee object', () => {
-    const employee = new Employee("Kartik")
+test("creates employee object", () => {
+    const employee = new Employee("Kartik", 1, "kartikinpublic@gmail.com")
     expect(employee.name).toBe("Kartik")
     expect(employee.id).toEqual(expect.any(Number))
-    expect(employee.email).stringMatching("@")
+    expect(employee.email).toMatch("@")
+    console.log(employee)
 })
 
 test("gets an employee's name", () => {
-    const employee = new Employee("Kartik")
+    const employee = new Employee("Kartik", 1, "kartikinpublic@gmail.com")
     expect(employee.getName()).toBe("Kartik")
 })
 
 test("gets an employee's id", () => {
-    const employee = new Employee("Kartik")
+    const employee = new Employee("Kartik", 1, "kartikinpublic@gmail.com")
     expect(employee.getId()).toEqual(expect.any(Number))
 })
 
 test("gets an employee's email", () => {
-    const employee = new Employee("Kartik")
-    expect(employee.getEmail()).stringMatching("@")
+    const employee = new Employee("Kartik", 1, "kartikinpublic@gmail.com")
+    expect(employee.getEmail()).toMatch("@")
 })
 
 test("gets an employee's role", () => {
-    const employee = new Employee("Kartik")
-    expect(employee.role()).toEqual("Employee")
+    const employee = new Employee("Kartik", 1, "kartikinpublic@gmail.com")
+    expect(employee.getRole()).toEqual("Employee")
 })
